6912a82674064a3b0dee0784396749290e13e565
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-03-21 11:11:55 +0900

    Update Jim-and-the-Orders.cs

--- Jim-and-the-Orders.cs
+++ Jim-and-the-Orders.cs
@@ -25,3 +25,28 @@ class Result
     public static List<int> jimOrders(List<List<int>> orders)
     {}
 }
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> orders = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            orders.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(ordersTemp => Convert.ToInt32(ordersTemp)).ToList());
+        }
+
+        List<int> result = Result.jimOrders(orders);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
