9ef594bda8db30a0acba43c79a8697a37f3e7757
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-03-27 07:55:56 +0900

    Update Jim-and-the-Orders.java

--- Jim-and-the-Orders.java
+++ Jim-and-the-Orders.java
@@ -10,7 +10,6 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-
 class Result {
 
     /*
@@ -36,10 +35,8 @@ class Result {
             result.add(x[1]);
 
         return result;
-    }    
-
-
-
+    }
+    
     
     public static List<Integer> jimOrders4(List<List<Integer>> orders) {
         PriorityQueue<int[]> pq = new PriorityQueue<>(
@@ -61,5 +58,41 @@ class Result {
         }
 
         return result;
-    }        
+    }    
+}
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> orders = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                orders.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = Result.jimOrders(orders);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
