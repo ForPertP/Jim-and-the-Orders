cb48ef75fa79394f38b035dfddfc7d382d4f8535
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-04-01 08:18:15 +0900

    Define jimOrders function signature
    Added function signature for jimOrders.

--- Jim-and-the-Orders.go
+++ Jim-and-the-Orders.go
@@ -10,3 +10,11 @@ import (
     "sort"
 )
 
+/*
+ * Complete the 'jimOrders' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts 2D_INTEGER_ARRAY orders as parameter.
+ */
+
+func jimOrders(orders [][]int32) []int32 {}
